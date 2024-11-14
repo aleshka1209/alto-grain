3483d88173ba2ea0b3e0505a90867c3edd432cb2
Alisher Turubekov <develepor@mail.ru>
2024-11-14 23:50:25 +0500

    products news procurement

--- procurement.php
+++ procurement.php
@@ -2,7 +2,30 @@
     include ('header.php');
 ?>
 
-    <h1>Закупки</h1>
+    <div class="procurement">
+        <div class="container">
+            <h2 class="procurement__title">Закупки</h2>
+            <hr class="procurement__hr">
+            <div class="procurement__block">
+                <div class="procurement__img">
+                    <img src="img/procurement/1.jpg" alt="1">
+                </div>
+                <div class="procurement__info">
+                    <p>
+                    Мы открыты сотрудничеству с сельхозпроизводителями Казахстана, реализующими продукцию в промышленных объемах. Условия сотрудничества обсуждаются в индивидуальном порядке – просто свяжитесь с нами!
+                    </p>
+                    <h4 class="procurement__h4">Что нужно сделать?</h4>
+                    <ul class="procurement__list">
+                        <li>Позвоните нам по телефону +7 7172 955-490 или напишите на адрес электронной почты info@alto-grain.kz</li>
+                        <li>Предоставьте необходимые документы, подтверждающие объем и качество товара, чтобы мы могли все проверить</li>
+                        <li>Подпишите договор купли-продажи</li>
+                        <li>Получите оплату за товар</li>
+                    </ul>
+                    <h4 class="procurement__h4">Мы всегда рада вашим звонкам и письмам!</h4>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php
     include ('footer.php');
